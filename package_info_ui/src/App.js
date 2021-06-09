f175befc51f70d1c869e4af689a9777a1b50ca3c
John Pap <giannispapad96@gmail.com>
2021-06-09 04:01:24 +0300

    Removed unnecessary template code from React project

--- package_info_ui/src/App.js
+++ package_info_ui/src/App.js
@@ -1,23 +1,12 @@
-import './App.css';
+import './App.scss';
+import { Button } from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Button>hello world</Button>
+        </div>
+    );
 }
 
 export default App;
