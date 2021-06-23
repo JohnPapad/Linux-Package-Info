3542830c142896b4fae3d9d456cbc38042ab4b79
John Pap <giannispapad96@gmail.com>
2021-06-23 05:31:13 +0300

    Added BrowserRouter in React project

--- package_info_ui/src/index.js
+++ package_info_ui/src/index.js
@@ -4,14 +4,18 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+
+const app = (
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
+ReactDOM.render(app, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
