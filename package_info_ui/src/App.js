71be8b066d81c55ba853a36bacd3aa8240259aa2
John Pap <giannispapad96@gmail.com>
2021-06-27 19:55:23 +0300

    Removed state and searchQueryHandler function from App React component
    the same logic will be applied in PackagesList component

--- package_info_ui/src/App.js
+++ package_info_ui/src/App.js
@@ -2,33 +2,14 @@ import { Component } from 'react';
 import './App.scss';
 import PackagesList from './components/PackagesList/PackagesList';
 import SearchForm from './components/SearchForm/SearchForm';
-import dpkgSampleData from './assets/packageData/dpkg-sample-data.json';
-import produce from 'immer';
 import { Container, Row, Col } from 'reactstrap';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 
 class App extends Component {
 
-    state = {
-        data: dpkgSampleData
-    };
-
-    searchQueryHandler = (query, selectedDistro) => {
-        this.setState(
-            produce(draft=>{
-                if (selectedDistro === "all") {
-                    draft.data = dpkgSampleData.filter(item=>item["package"].includes(query));
-                }
-                else {
-                    draft.data = dpkgSampleData.filter(item=>item["distro"] === selectedDistro && item["package"].includes(query));
-                }
-            })
-        );
-    }
-
+   
     render() {
-        // console.log("data", this.state)
         return (
 
             <Switch>
@@ -36,25 +17,16 @@ class App extends Component {
                     <Container fluid className="mt-5">
                         <Row>
                             <Col md="3">
-                                <SearchForm
-                                    searchQueryHandler={this.searchQueryHandler}
-                                />
+                                <SearchForm/>
                             </Col>
                         </Row>
-                        <PackagesList
-                            data={this.state.data}
-                        />
+                        <PackagesList/>
                     </Container>
                 </Route>
 
                 <Redirect to="/" />
             </Switch>
         );
-    }
-
-
-    componentDidMount () {
-        // API call here to fetch packages info
     }
     
 }
