024bff6053aa370905b3a5ba2ae7556075a19e16
John Pap <giannispapad96@gmail.com>
2021-07-01 04:25:08 +0300

    Removed SearchForm from App React component

--- package_info_ui/src/App.js
+++ package_info_ui/src/App.js
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import './App.scss';
 import PackagesList from './components/PackagesList/PackagesList';
-import SearchForm from './components/SearchForm/SearchForm';
 import { Container, Row, Col } from 'reactstrap';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
@@ -15,11 +14,6 @@ class App extends Component {
             <Switch>
                 <Route path="/" exact>
                     <Container fluid className="mt-5">
-                        <Row>
-                            <Col md="3">
-                                <SearchForm/>
-                            </Col>
-                        </Row>
                         <PackagesList/>
                     </Container>
                 </Route>
