549bf4bd81cb44e4bd41be4adc09ed5f3c15de8a
John Pap <giannispapad96@gmail.com>
2021-07-11 03:36:36 +0300

    Added exporting packages to JSON file functionality in collector script

--- package_info_collector/collector.py
+++ package_info_collector/collector.py
@@ -1,6 +1,8 @@
 import os
 import ray
 import sys
+import json
+
 
 info_to_parse = set(["Version", "Section", "Installed-Size", "Homepage", "Description", "Bugs"])
 
@@ -8,20 +10,24 @@ def parse_packages_info():
     packages_output = (os.popen('apt-cache search .')).readlines()
     print("Number of packages found: ", len(packages_output))
 
-    packages = {}
     ids = []
     ray.init()
     for line in packages_output:
-        id = parallel_processing.remote(packages, line)
+        id = parallel_processing.remote(line)
         ids.append(id)
 
-    ray.get(ids)
- 
+    packages = ray.get(ids)
+    print("Number of packages parsed: ", len(packages))
+    with open('data.json', 'w') as f:
+        json.dump(packages, f)
+
 
 @ray.remote
-def parallel_processing(packages, line):
+def parallel_processing(line):
     package_name = line.rstrip('\n').split(" - ")[0]
     print(f"package: '{package_name}'")
+    package = {}
+    package[package_name] = {}
 
     versions = {}
     versions_output = (os.popen(f'apt-cache madison {package_name}')).readlines()
@@ -29,15 +35,17 @@ def parallel_processing(packages, line):
         _, version, arch = version_line.rstrip('\n').split(" | ")
         version = version.lstrip(" ")
         arch = arch.split(" ")[-2]
-        assert arch == "amd64" or arch == "i386", "parsing error in package version's architecture"
+        # assert arch == "amd64" or arch == "i386", "parsing error in package version's architecture"
         # print(f"version: '{version}' - '{arch}'")
         if version not in versions:
             versions[version] = set()
 
         versions[version].add(arch)
 
-    packages[package_name] = {}
-    packages[package_name]["versions"] = versions
+    for a, b in versions.items():
+        versions[a] = list(b)
+
+    package[package_name]["versions"] = versions
 
     info = {}
     info_output = (os.popen(f'apt show {package_name}')).readlines()
@@ -55,7 +63,9 @@ def parallel_processing(packages, line):
             info_value = splitted_info[1]
             info[info_key] = info_value
 
-    packages[package_name]["info"] = info
+    package[package_name]["info"] = info
+
+    return package
 
 
 if __name__ == "__main__":
