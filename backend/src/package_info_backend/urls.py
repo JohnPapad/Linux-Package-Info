07b75e5df3065e39213604f2e4059c562fb43fd5
John Pap <giannispapad96@gmail.com>
2021-07-26 07:44:16 +0300

    Added PackageViewSet router in Django urls

--- backend/src/package_info_backend/urls.py
+++ backend/src/package_info_backend/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from rest_framework.routers import SimpleRouter
+from api import views
+
+
+package_router = SimpleRouter()
+package_router.register(r'packages', views.PackageViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^api/v1/', include(package_router.urls))
 ]
