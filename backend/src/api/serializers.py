687873f5c39601fdb44dc84be54c6ccfc59e3d6e
John Pap <giannispapad96@gmail.com>
2021-07-26 08:29:48 +0300

    Added overloaded methods in Django PackageSerializer

--- backend/src/api/serializers.py
+++ backend/src/api/serializers.py
@@ -6,10 +6,29 @@ class PackageVersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = PackageVersion
         fields= "__all__"
+        # exclude = ["package"]
+        extra_kwargs = {
+            'package': { 'read_only': True }
+        }
 
 
 class PackageSerializer(serializers.ModelSerializer):
     versions = PackageVersionSerializer(many=True)
+
+    def validate_versions(self, versions):
+        if len(versions) == 0:
+            raise serializers.ValidationError("This field must not be empty.")
+            
+        return versions
+
     class Meta:
         model = Package
         fields= "__all__"
+
+    def create(self, validated_data):
+        versions = validated_data.pop('versions')
+        pkg = Package.objects.create(**validated_data)
+        for version in versions:
+            PackageVersion.objects.create(package=pkg, **version)
+
+        return pkg
