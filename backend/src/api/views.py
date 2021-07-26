5a0caf3a6551ffd0bb78cf49d602ad321db6f809
John Pap <giannispapad96@gmail.com>
2021-07-26 07:42:39 +0300

    Added PackageViewSet in Django views

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .serializers import PackageVersionSerializer, PackageSerializer
+from rest_framework import viewsets
+from . import models
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class PackageViewSet(viewsets.ModelViewSet):
+    queryset = models.Package.objects.all()
+    serializer_class = PackageSerializer
+    permission_classes = [AllowAny]
