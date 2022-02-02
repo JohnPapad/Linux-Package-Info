4e900f52024f84d0a0ba6ecab94bf2e0853520d1
John Pap <giannispapad96@gmail.com>
2022-02-02 05:58:49 +0200

    Added SessionAuthentication in DEFAULT_AUTHENTICATION_CLASSES in Django
    to support login in DRF's API admin panel

--- backend/src/users/admin.py
+++ backend/src/users/admin.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
+
 class MyUserAdmin(UserAdmin):
     list_display = ['id', 'username', 'email', 'is_staff', 'is_superuser', 'is_active']
+
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
