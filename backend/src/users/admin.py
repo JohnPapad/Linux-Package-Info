9c3378c4d4310f1cfc8a5a3ce16d8a383acbd17a
John Pap <giannispapad96@gmail.com>
2022-02-02 04:50:55 +0200

    Changed User's props list display in admin panel in Django project

--- backend/src/users/admin.py
+++ backend/src/users/admin.py
@@ -1,2 +1,11 @@
 # Copyright 2021 Ioannis Papadopoulos
 # SPDX-License-Identifier: AGPL-3.0-or-later
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+class MyUserAdmin(UserAdmin):
+    list_display = ['id', 'username', 'email', 'is_staff', 'is_superuser', 'is_active']
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
