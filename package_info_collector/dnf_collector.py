a1906446b6de7230c339b8e3e72ebcdd01dc82a2
John Pap <giannispapad96@gmail.com>
2022-02-05 22:11:03 +0200

    Fixed package results parsing in fetch_package_info in dnf collector
    also added feedback messages when HTTP calls to backend API fail

--- package_info_collector/dnf_collector.py
+++ package_info_collector/dnf_collector.py
@@ -390,12 +390,14 @@ def fetch_package_info(distro, base_URL, package_name):
     }
 
     try:
-        response = requests.get(f'{base_URL}/packages/', params=URL_params)
-        if response.status_code != 200:
+        res = requests.get(f'{base_URL}/packages/', params=URL_params)
+        if res.status_code != 200:
+            print(f"-> Fetching info of package '{package_name}' failed with response status code: {res.status_code}, response:\n{res.json()}")
+            print("-" * 30)
             return None
 
         # successfully retrieved package info
-        res_data = response.json() # expecting a list with 1 item max
+        res_data = res.json()['results'] # expecting a list with 1 item max
         return res_data[0]
     except:
         pass
@@ -403,7 +405,7 @@ def fetch_package_info(distro, base_URL, package_name):
     return None
 
 
-def add_new_versions_to_existing_package(base_URL, pkg_versions_to_add, package_id):
+def add_new_versions_to_existing_package(base_URL, pkg_versions_to_add, package_id, package_name):
     if len(pkg_versions_to_add) == 0:
         return False
 
@@ -411,6 +413,9 @@ def add_new_versions_to_existing_package(base_URL, pkg_versions_to_add, package_
         res = JWTAuth_obj.session.post(f'{base_URL}/packages/{package_id}/versions/', json=pkg_versions_to_add)
         if res.status_code == 201:
             return True
+
+        print(f"-> Adding new versions to existing package '{package_name}' failed with response status code: {res.status_code}, response:\n{res.json()}")
+        print("-" * 30)
     except:
         pass
 
@@ -432,7 +437,9 @@ def add_new_package(distro, base_URL, package_name, package_info, pkg_versions_t
         res = JWTAuth_obj.session.post(f'{base_URL}/packages/', json=package_info)
         if res.status_code == 201:
             return True
-        print("status: ", res.status_code, res.json())
+
+        print(f"-> Adding new package '{package_name}' failed with response status code: {res.status_code}, response:\n{res.json()}")
+        print("-" * 30)
     except:
         pass
 
@@ -487,7 +494,7 @@ def parallel_processing(distro, base_URL, package_name, distro_archives_URL, dis
                 "binary_URL": pkg_version_binary_URL
             })
 
-        new_versions_flag = add_new_versions_to_existing_package(base_URL, pkg_versions_to_add, pkg_existing_info["id"]) # add only the possible new versions
+        new_versions_flag = add_new_versions_to_existing_package(base_URL, pkg_versions_to_add, pkg_existing_info["id"], package_name) # add only the possible new versions
         if new_versions_flag:
             report_msg += "successfully added new versions "
         else:
