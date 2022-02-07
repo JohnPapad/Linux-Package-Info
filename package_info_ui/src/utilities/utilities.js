1128180be03cb8b011ed9d9617f5f5dddc0535c5
John Pap <giannispapad96@gmail.com>
2022-02-07 06:42:44 +0200

    Added mapDistroToBootstrapColor in utilities file in React project

--- package_info_ui/src/utilities/utilities.js
+++ package_info_ui/src/utilities/utilities.js
@@ -5,3 +5,27 @@ export const removeHttp = (url) => {
         replace("http://", "").
         replace("www.", "");
 }
+
+export const mapDistroToBootstrapColor = (distro) => {
+    if (distro.startsWith("Ubuntu")) {
+        return "warning"
+    }
+    
+    if (distro.startsWith("Debian")) {
+        return "danger"
+
+    }
+    
+    if (distro.startsWith("Kali")) {
+        return "dark"
+    }
+
+    if (distro.startsWith("Fedora")) {
+        return "primary"
+
+    }
+
+    if (distro.startsWith("CentOS")) {
+        return "info"
+    }
+}
