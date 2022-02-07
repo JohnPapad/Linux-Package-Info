be46ce1fb2b43e9326f613ab9c81fc414827d141
John Pap <giannispapad96@gmail.com>
2022-02-07 20:26:50 +0200

    Refactored PackageList React component
    also deleted non used code

--- package_info_ui/src/components/PackagesList/PackagesList.js
+++ package_info_ui/src/components/PackagesList/PackagesList.js
@@ -1,27 +1,30 @@
 import { Component } from "react"
 import DataTable from 'react-data-table-component';
 import { withRouter } from 'react-router-dom';
-import { orderBy } from 'lodash';
 import produce from 'immer';
 import { isEqual } from 'lodash';
-import { getQueryParams } from '../../utilities/URIutil';
-import dpkgSampleData from '../../assets/packageData/dpkg-sample-data.json';
 import SearchForm from '../SearchForm/SearchForm';
-import { Row, Col, Button } from 'reactstrap';
+import { Row, Col, Button, Spinner } from 'reactstrap';
 import ExportOptions from '../ExportOptions/ExportOptions';
 import { downloadCSV } from '../../utilities/CSVutil';
 import { downloadJSON } from '../../utilities/JSONutil';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
-
-
-const baseSWHul = "https://archive.softwareheritage.org/browse/";
+import { API } from '../../services/API';
+import queryString from 'query-string';
+import validUrl from 'valid-url';
+import { removeHttp, mapDistroToBootstrapColor } from "../../utilities/utilities";
+import VersionsList from "./VersionsLIst/VersionsList";
+import { packagesDataTableStyles } from './ContentTableStyles';
 
 
 class PackagesList extends Component {
 
     state = {
-        data: dpkgSampleData,
+        data: [],
+        tableIsLoading: true,
+        dataTotalCount: 0,
+        expandedRowIds: {},
         visibleColumns: ['type', 'category', 'rating', 'license', 'maintainer', 'website', 'repo', 'description']
     };
 
@@ -34,67 +37,6 @@ class PackagesList extends Component {
         else if (type === "JSON") {
             downloadJSON(this.selectedPackages);
         }
-    }
-
-    filterPackages = () => {
-        const URLparams = getQueryParams(this.props.location.search);
-        if ((!URLparams.distro) || (!URLparams.package)) {
-            return;
-        }
-
-        this.setState(
-            produce(draft=>{
-                if (URLparams.distro === "all") {
-                    draft.data = dpkgSampleData.filter(item=>item["package"].includes(URLparams.package));
-                }
-                else {
-                    draft.data = dpkgSampleData.filter(item=>item["distro"] === URLparams.distro && item["package"].includes(URLparams.package));
-                }
-            })
-        );
-    }
-
-    handleSort = (rows, selector, sortDirection) => {
-        // console.log("rows", rows)
-        // console.log("selector:", selector)
-        // console.log("direction: ", sortDirection)
-  
-        if (selector === "distro") {
-            return [...this.sortByDistro(rows, selector, sortDirection)];
-        }
-        return orderBy(rows, selector, sortDirection);
-    }
-
-    sortByDistro = (rows, selector, sortDirection) => {
-        const sortMethod = (rowA, rowB) => {
-            if (rowA[selector] < rowB[selector]) {
-                return sortDirection === "asc" ? -1 : 1;
-            }
-            else if (rowA[selector] > rowB[selector]) {
-                return sortDirection === "asc" ? 1 : -1;
-            }
-            
-            if (rowA["package"] < rowB["package"]) {
-                return -1;
-            }
-            else if (rowA["package"] > rowB["package"]) {
-                return 1;
-            }
-
-            if (rowA["version"] < rowB["version"]) {
-                return 1;
-            }
-            else if (rowA["version"] > rowB["version"]) {
-                return -1;
-            }
-            
-            return 0;
-        } 
-        return rows.sort(sortMethod);
-    }
-
-    sortByPackage = () => {
-
     }
 
     selectedRowsChangedHandler = state => {
@@ -118,6 +60,28 @@ class PackagesList extends Component {
         );
     }
 
+    onRowExpandToggled = (expanded, row) => {
+        console.log("expanded", expanded, "row", row)
+        this.setState(
+            produce(draft=>{
+                if (expanded) {
+                    draft.expandedRowIds[row.id] = true;
+                }
+                else {
+                    delete draft.expandedRowIds[row.id];
+                }
+            })
+        );
+    }
+
+    handlePackagePageChange = (page) => {
+        alert(page)
+    }
+
+    handleSort = async (column, sortDirection) => {
+        console.log("handle sort: ", column.id, sortDirection)
+    }
+
     render() {
 
         console.log("packages rendered")
@@ -125,49 +89,72 @@ class PackagesList extends Component {
         const visibleColumns = new Set(this.state.visibleColumns);
         const columns = [
             {
+                id: 'name',
                 name: "Package",
-                selector: "name",
+                selector: row => row['name'],
                 sortable: true,
-                grow: 1.5
+                grow: 2,
+                wrap: true,
+                style: {
+                    fontWeight: "500", 
+                    fontSize: "110%",
+                    color: "#6c757d"
+                },
             },
             {
+                id: 'distro',
                 name: 'Distribution',
-                selector: 'distro',
+                selector: row => row['distro'],
                 sortable: true,
                 reorder:true,
-                grow: 1
+                // grow: 1,
+                // center: true,
+                style: {
+                    fontWeight: "500"
+                },
+                cell: row=>(
+                    <div className={`my-auto p-1 border rounded border-${mapDistroToBootstrapColor(row.distro)} text-${mapDistroToBootstrapColor(row.distro)}`}>
+                        {row.distro}
+                    </div>
+                )
             },
         
             {
+                id: 'type',
                 name: "Type",
-                selector: "type",
-                sortable: true,
-                reorder:true,
-                omit: !visibleColumns.has('type'),
-                grow: 0.1
-            },
-            {
-                name: "Category",
-                selector: "section",
-                sortable: true,
-                reorder:true,
-                omit: !visibleColumns.has('category'),
-                grow: 0.5
-            },
-            {
-                name: "Rating",
-                selector: "rating",
+                selector: row => row['type'],
                 sortable: true,
                 reorder:true,
                 // center: true,
+                width: "80px",
+                omit: !visibleColumns.has('type'),
+            },
+            {
+                id: 'section',
+                name: "Category",
+                selector: row => row['section'],
+                sortable: true,
+                reorder:true,
+                // center: true,
+                omit: !visibleColumns.has('category'),
+                // grow: 0.5
+            },
+            {
+                id: 'rating',
+                name: "Rating",
+                selector: row => row['rating'],
+                sortable: true,
+                reorder:true,
+                // center: true,
+                width: "95px",
                 omit: !visibleColumns.has('rating'),
-                grow: 0.1,
                 cell: row=>row.rating ? row.rating.toFixed(1) : "-"
             },
 
             {
+                id: 'license',
                 name: "License",
-                selector: "license",
+                selector: row => row['license'],
                 reorder:true,
                 omit: !visibleColumns.has('license'),
                 grow: 2,
@@ -175,40 +162,44 @@ class PackagesList extends Component {
             },
 
             {
+                id: 'maintainer',
                 name: "Maintainer",
-                selector: "maintainer",
+                selector: row => row['maintainer'],
                 reorder:true,
                 omit: !visibleColumns.has('maintainer'),
                 grow: 2,
                 cell: row=>{
                     if (validUrl.isUri(row.maintainer)) {
-                        return <a target="_blank" rel="noreferrer" href={row.maintainer}>{row.maintainer.replace("https://", "")}</a>
+                        return <a target="_blank" rel="noreferrer" href={row.maintainer}>{removeHttp(row.maintainer)}</a>;
                     }
-                    return row.maintainer;
+                    return <div className="py-1"> {row.maintainer} </div>;
                 }
             },
 
             {
+                id: 'homepage',
                 name: "Website",
-                selector: "homepage",
+                selector: row => row['homepage'],
                 reorder:true,
                 omit: !visibleColumns.has('website'),
                 grow: 1.5,
-                format: row => <a target="_blank" rel="noreferrer" href={row.homepage}>{row.homepage.replace("https://", "").replace("http://", "")}</a>
+                format: row => <a target="_blank" rel="noreferrer" href={row.homepage}>{removeHttp(row.homepage)}</a>
             },
         
             {
+                id: 'repo_URL',
                 name: "Code Repository",
-                selector: "repo_URL",
+                selector: row => row['repo_URL'],
                 reorder:true,
                 omit: !visibleColumns.has('repo'),
                 grow: 1.5,
-                format: row => <a target="_blank" rel="noreferrer" href={row.repo_URL}>{row.repo_URL.replace("https://", "")}</a>
+                format: row => <a target="_blank" rel="noreferrer" href={row.repo_URL}>{removeHttp(row.repo_URL)}</a>
             },
 
             {
+                id: 'description',
                 name: "Description",
-                selector: "description",
+                selector: row => row['description'],
                 reorder:true,
                 omit: !visibleColumns.has('description'),
                 wrap: true,
@@ -217,20 +208,29 @@ class PackagesList extends Component {
         
         ];
 
+        const expandedRowStyles = [
+            {
+                when: row => this.state.expandedRowIds.hasOwnProperty(row.id),
+                style: {
+                    backgroundColor: 'rgba(0,0,0,.12)',
+                },
+            },
+        ];
+
         return (
             <>
             <Row className="mb-3 justify-content-between">
                 <Col md="3" className="py-2">
                     <SearchForm/>
                 </Col>
-                <Col xs="auto" className="py-2">
+                <Col xs="auto" className="py-2 align-items-center d-flex">
                     <span className="fw-bold me-2">
                         Show: 
                     </span>
                     {
                         this.state.visibleColumns.map(col=>(
                             <span key={col} className="mx-2">
-                                <Button color="dark" outline disabled>
+                                <Button color="dark" outline disabled size="sm">
                                     <span className="fw-bold">
                                         {col}
                                     </span>
@@ -241,7 +241,7 @@ class PackagesList extends Component {
 
                         ))
                     }
-                    <Button color="secondary" className="ms-2" onClick={this.showAllColumnsHandler}>
+                    <Button color="secondary" className="ms-2" size="sm" onClick={this.showAllColumnsHandler}>
                         <span className="fw-bold">
                             All
                         </span>
@@ -249,30 +249,70 @@ class PackagesList extends Component {
                 </Col>
             </Row>
             <DataTable
+                customStyles={packagesDataTableStyles}
+                conditionalRowStyles={expandedRowStyles}
                 data={[...this.state.data]}
                 columns={columns}
-                defaultSortField="distro"
+                // defaultSortField="distro"
+                fixedHeader
                 highlightOnHover
-                pagination
                 responsive
+                progressPending={this.state.tableIsLoading}
+                // progressComponent={(
+                //     <div  style={{height: "15vh", fontSize: "150%"}}>
+
+                //         <Spinner color="secondary" style={{width: "10%"}}/>
+                //     </div>
+                // )}
+                pagination
+                paginationServer
+			    paginationTotalRows={this.state.dataTotalCount}
+                paginationPerPage={10}
+                paginationComponentOptions={{
+                    noRowsPerPage: true
+                }}
+                onChangePage={this.handlePackagePageChange}
+                expandableRows
+                expandOnRowClicked
+                expandableRowsComponent={({data})=><VersionsList versions={data.versions}/>} 
                 selectableRows
                 selectableRowsHighlight
-                sortFunction={this.handleSort}
+                sortServer
+			    onSort={this.handleSort}
                 onSelectedRowsChange={this.selectedRowsChangedHandler}
+                onRowExpandToggled={this.onRowExpandToggled}
                 contextActions={<ExportOptions exportHandler={this.exportHandler}/>}
             />
             </>
         );
     }
 
+    fetchPackages = async (query) => {
+        const response = await API.getPackages(query);
+        console.log(response);
+        this.setState(
+            produce(draft=>{
+                draft.tableIsLoading=false;
+                draft.data = response.results;
+                draft.dataTotalCount = response.count;
+            })
+        );
+    }
+
     componentDidMount () {
         console.log("Package list did mount");
-        this.filterPackages();
+        const query = queryString.stringify({
+            ordering: "-avg_rating"
+        });
+        console.log("query", query)
+        // this.filterPackages();
+        this.fetchPackages(query);
+
     }
 
     componentDidUpdate () {
         console.log("Package list did update");
-        this.filterPackages();
+        // this.filterPackages();
     }
 
     shouldComponentUpdate (nextProps, nextState) {
