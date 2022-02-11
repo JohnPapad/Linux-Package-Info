5c092297880efa371171368845edd581add89cc5
John Pap <giannispapad96@gmail.com>
2022-02-11 19:02:31 +0200

    Added Auth modal in Navbar React component
    renamed navabar's state property, handler

--- package_info_ui/src/components/Navbar/Navbar.js
+++ package_info_ui/src/components/Navbar/Navbar.js
@@ -3,44 +3,56 @@ import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Na
 import { NavLink as RouterNavLink, withRouter }  from 'react-router-dom';
 import produce  from 'immer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSignInAlt, faSignOutAlt, faUserEdit, faListOl, faUserCircle, faArrowRightToBracket, faBoxArchive, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
-
+import { faSignOutAlt, faUserEdit, faArrowRightToBracket, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import Auth from '../Auth/Auth';
 
 
  class NavigatonBar extends Component {
 
     state = {
-        isOpen: false
+        navbarIsOpen: false,
+        authModalIsOpen: false,
+        authModalType: null,
     }
 
-    toggle = () => {
+    toggleNavbar = () => {
         this.setState(
             produce(draft=>{
-                draft.isOpen = !draft.isOpen;
+                draft.navbarIsOpen = !draft.navbarIsOpen;
+            })
+        );
+    }
+
+    toggleAuthModal = (type) => {
+        this.setState(
+            produce(draft=>{
+                draft.authModalIsOpen = !draft.authModalIsOpen;
+                draft.authModalType = type;
             })
         );
     }
 
     render () {
-        const isLoggedIn = false;
+        const isLoggedIn = localStorage.getItem("access_token") ? true : false;
         return (
+            <>
             <Navbar color="secondary" dark expand="sm" className="fixed-top">
                 <NavbarBrand className='fw-bold ms-4 border border-2 rounded px-2 py-0'>
                     PKGman
                     <FontAwesomeIcon icon={faFolderOpen} className="ms-2 small"/>
                 </NavbarBrand>
-                <NavbarToggler onClick={this.toggle}/>
-                <Collapse isOpen={this.state.isOpen} navbar className='mb-1'>
+                <NavbarToggler onClick={this.toggleNavbar}/>
+                <Collapse isOpen={this.state.navbarIsOpen} navbar className='mb-1'>
                     <Nav className="mr-auto" navbar className="ms-auto">
                         {
                             !isLoggedIn ?
                                 <>
-                                    <Button size="sm" color='primary' className='fw-bold'>
-                                        <span className='border-end pe-1'>Sign in</span>
+                                    <Button size="sm" color='primary' className='fw-bold' onClick={()=>this.toggleAuthModal("login")}>
+                                        <span className='border-end pe-1'>Log in</span>
                                         <FontAwesomeIcon icon={faArrowRightToBracket} className="ms-1 small"/>
                                     </Button>
                                
-                                    <Button size="sm" color='info' className='fw-bold pe-1 ms-2'>
+                                    <Button size="sm" color='info' className='fw-bold pe-1 ms-2' onClick={()=>this.toggleAuthModal("signup")}>
                                         <span className='border-end pe-1 border-dark'>Sign up</span>
                                         <FontAwesomeIcon icon={faUserEdit} className="ms-1 small"/>
                                     </Button>
@@ -53,7 +65,12 @@ import { faSignInAlt, faSignOutAlt, faUserEdit, faListOl, faUserCircle, faArrowR
                         }
                     </Nav>
                 </Collapse>
-          </Navbar>
+            </Navbar>
+
+            <Modal size="md" isOpen={this.state.authModalIsOpen} toggle={this.toggleAuthModal} className='pt-5'>
+                <Auth type={this.state.authModalType} toggleAuthModal={this.toggleAuthModal}/>
+            </Modal>
+          </>
         );
     }
 
