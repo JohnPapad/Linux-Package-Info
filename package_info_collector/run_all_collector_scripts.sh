e9d6e923ade3c58ee7315db12a0b1003bd94a0c1
John Pap <giannispapad96@gmail.com>
2022-02-13 04:10:11 +0200

    Changed run_all_collector_scripts to work with collector Docker service

--- package_info_collector/run_all_collector_scripts.sh
+++ package_info_collector/run_all_collector_scripts.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
+# Copyright 2021 Ioannis Papadopoulos
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
-GITHUB_TOKEN="" # your github account token - fill it out
+# vars
+DOCKER_VOL_MOUNT="-v /var/run/docker.sock:/var/run/docker.sock"
+WORK_DIR="/collector" # must be indentical to collector Dockerfile's 'WORKDIR'
 SHM_SIZE="5.07gb" # considering that your machine has 16 GB of RAM - change it accordingly otherwise
 BACKEND_API="http://localhost:8000/api/v1" # considering that the backend runs on the same machine - change it accordingly otherwise
+GITHUB_TOKEN="" # your github account token - fill it out
 USERNAME="" # your credentials to login to backend API - fill it out
 PASSWORD="" # your credentials to login to backend API - fill it out
 
 # Ubuntu:
-docker build -t ubuntu_distro -f ubuntu.Dockerfile .
-docker run --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD ubuntu_distro python3 apt_collector.py -d Ubuntu -u $BACKEND_API -a http://archive.ubuntu.com/ubuntu
+docker build -t ubuntu_distro -f $WORK_DIR/ubuntu.Dockerfile $WORK_DIR
+docker run --rm $DOCKER_VOL_MOUNT --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD ubuntu_distro python3 apt_collector.py -d Ubuntu -u $BACKEND_API -a http://archive.ubuntu.com/ubuntu
 
 # Debian:
-docker build -t debian_distro -f debian.Dockerfile .
-docker run --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD debian_distro python3 apt_collector.py -d Debian -u $BACKEND_API -a http://ftp.debian.org/debian
+docker build -t debian_distro -f $WORK_DIR/debian.Dockerfile $WORK_DIR
+docker run --rm $DOCKER_VOL_MOUNT --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD debian_distro python3 apt_collector.py -d Debian -u $BACKEND_API -a http://ftp.debian.org/debian
 
 # Kali:
-docker build -t kali_distro -f kali.Dockerfile .
-docker run --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD kali_distro python3 apt_collector.py -d Kali -u $BACKEND_API -a https://http.kali.org
+docker build -t kali_distro -f $WORK_DIR/kali.Dockerfile $WORK_DIR
+docker run --rm $DOCKER_VOL_MOUNT --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD kali_distro python3 apt_collector.py -d Kali -u $BACKEND_API -a https://http.kali.org
 
-# Fedora:
-docker build -t fedora_distro -f fedora.Dockerfile .
-docker run --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD fedora_distro python3 dnf_collector.py -d Fedora -u $BACKEND_API -a https://kojipkgs.fedoraproject.org/packages -r https://src.fedoraproject.org/api/0/rpms
+#Fedora:
+docker build -t fedora_distro -f $WORK_DIR/fedora.Dockerfile $WORK_DIR
+docker run --rm $DOCKER_VOL_MOUNT --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD fedora_distro python3 dnf_collector.py -d Fedora -u $BACKEND_API -a https://kojipkgs.fedoraproject.org/packages -r https://src.fedoraproject.org/api/0/rpms
 
 # CentOS:
-docker build -t centos_distro -f centos.Dockerfile .
-docker run --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD centos_distro python3 dnf_collector.py -d CentOS -u $BACKEND_API -a https://vault.centos.org/8.4.2105 -r https://git.centos.org/api/0/rpms
+docker build -t centos_distro -f $WORK_DIR/centos.Dockerfile $WORK_DIR
+docker run --rm $DOCKER_VOL_MOUNT --net host --shm-size=$SHM_SIZE --env GITHUB-TOKEN=$GITHUB_TOKEN --env USERNAME=$USERNAME --env PASSWORD=$PASSWORD centos_distro python3 dnf_collector.py -d CentOS -u $BACKEND_API -a https://vault.centos.org/8.4.2105 -r https://git.centos.org/api/0/rpms
